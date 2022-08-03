7dfb33164516656c15e1c25f5dcf7a006e26d8f6
mustafa <mustafatato1@hotmail.com>
2022-08-03 13:55:35 +0100

    vendor registeratiom

--- kulonline_main/settings.py
+++ kulonline_main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'vendor',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +85,7 @@ DATABASES = {
         'HOST': config('DB_HOST'),
     }
 }
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -125,7 +127,16 @@ STATICFILES_DIRS = [
     'kulonline_main/static'
 ]
 
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
---
+++ vendor/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from vendor.models import Vendor
+# from . models import User , UserProfile
+# from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'vendor_name', 'is_approved', 'created_at')
+    list_display_links = ('user', 'vendor_name')
+    # filter_horizontal = ()
+    # list_filter = ()
+    # fieldsets = ()
+
+
+
+admin.site.register(Vendor, VendorAdmin)
+# admin.site.register(UserProfile)
