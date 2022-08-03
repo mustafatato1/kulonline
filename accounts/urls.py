bc5ba5c2c7670af4068d7723eaa06ad5488055e9
mustafa <mustafatato1@hotmail.com>
2022-08-03 14:56:51 +0100

    vendor registeratiom

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('registerUser/', views.registerUser, name='registerUser'),
     path('registerVendor/', views.registerVendor, name='registerVendor'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -75,3 +75,27 @@ def registerVendor(request):
     }
 
     return render(request, 'accounts/registerVendor.html',context)
+
+def login(request):
+    # if request.user.is_authenticated:
+    #     messages.warning(request, 'You are already logged in!')
+    #     return redirect('myAccount')
+    # elif request.method == 'POST':
+    #     email = request.POST['email']
+    #     password = request.POST['password']
+
+    #     user = auth.authenticate(email=email, password=password)
+
+    #     if user is not None:
+    #         auth.login(request, user)
+    #         messages.success(request, 'You are now logged in.')
+    #         return redirect('myAccount')
+    #     else:
+    #         messages.error(request, 'Invalid login credentials')
+    #         return redirect('login')
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request, 'You are logged out.')
+    return redirect('login')
