280b75c64862d9d8844ac558f36df29c0fdda82e
mustafa <mustafatato1@hotmail.com>
2022-08-10 07:57:46 +0100

    forgot password feature

--- accounts/forms.py
+++ accounts/forms.py
@@ -31,11 +31,11 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
 
-#     def __init__(self, *args, **kwargs):
-#         super(UserProfileForm, self).__init__(*args, **kwargs)
-#         for field in self.fields:
-#             if field == 'latitude' or field == 'longitude':
-#                 self.fields[field].widget.attrs['readonly'] = 'readonly'
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
 
 
 class UserInfoForm(forms.ModelForm):
